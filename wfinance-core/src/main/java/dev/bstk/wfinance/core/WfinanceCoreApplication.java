b79c2cd331b9ad7a44da7fb834de69a294a0bed3
Bruno Luz <bsilva.se@gmail.com>
2021-03-06 18:22:33 -0300

    main - Criando projeto Core

---
+++ wfinance-core/src/main/java/dev/bstk/wfinance/core/WfinanceCoreApplication.java
@@ -0,0 +1,13 @@
+package dev.bstk.wfinance.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfinanceCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WfinanceCoreApplication.class, args);
+    }
+
+}
---
+++ wfinance-core/src/test/java/dev/bstk/wfinance/core/WfinanceCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bstk.wfinance.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WfinanceCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
